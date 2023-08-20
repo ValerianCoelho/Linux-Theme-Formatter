aa7cfdf3a08f35a6a0d02e0098548f31b572cd21
Valerian Coelho <valerian27012004@gmail.com>
2023-08-21 00:19:26 +0530

    Editor Theme's Added

---
+++ src/Themes/Editor/Theme.js
@@ -0,0 +1,11 @@
+export const Theme = {
+  outputEditorBgColor: '#EEEEEC',
+  outputEditorToolbarColor: '#E8E89F',
+  outputEditorTaskbarColor: '#E8E8D1',
+  outputEditorBorderColor: '#CFCF92',
+
+  kernelColor: '#66A116,',
+  folderColor: '#4869B0',
+  accentColor: '#CD0A13',
+  textColor: '#000000'
+}
