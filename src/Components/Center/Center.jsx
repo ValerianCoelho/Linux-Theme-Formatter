9d0db0ace4d1248bf98e7d3ca5d6667472101c6d
Valerian Coelho <valerian27012004@gmail.com>
2023-08-21 00:18:08 +0530

    Center Section Complete

--- src/Components/Center/Center.jsx
+++ src/Components/Center/Center.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import InputTextEditor from './Components/InputTextEditor/InputTextEditor'
+import OutputTextEditor from './Components/OutputTextEditor/OutputTextEditor'
+import { Theme } from "../../Themes/Webpage/Theme";
 
 function Center() {
+  const styles = {
+    center: {
+      display: 'flex',
+      justifyContent: 'space-evenly'
+    }
+  }
   return (
-    <>
-      <div className="input-text-editor"></div>
-      <div className="output-text-editor"></div>
-    </>
+    <div style={styles.center}>
+      <InputTextEditor/>
+      <OutputTextEditor/>
+    </div>
   )
 }
 
