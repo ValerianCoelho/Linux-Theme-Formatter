265bcbf962633f03bae21da92d938a7a2a1972ac
Valerian Coelho <valerian27012004@gmail.com>
2023-08-20 16:14:35 +0530

    Default Themes Added

---
+++ src/Themes/Webpage/Theme.js
@@ -0,0 +1,18 @@
+export const Theme = {
+  primaryColor: '#E5F1FB',
+
+  shareBtnBgColor: '#F2D1B5',
+  donateBtnBgColor: '#434BDF',
+
+  shareBtnColor: '#05131F',
+  donateBtnColor: '#F5F5F5',
+
+  inputEditorBgColor: '#E5F1FB',
+  outputEditorBgColor: '#EEEEEC',
+
+  inputEditorToolbarColor: '#65AAE6',
+  outputEditorToolbarColor: '#E8E89F',
+
+  inputEditorTaskbarColor: '#D9E6F0',
+  outputEditorTaskbarColor: '#E8E8D1'
+}
