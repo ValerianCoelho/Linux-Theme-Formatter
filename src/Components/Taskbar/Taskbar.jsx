7310fcb021a431432c2d60e3dc10e9ccc10e98e4
Valerian Coelho <valerian27012004@gmail.com>
2023-08-21 00:19:03 +0530

    Taskbar Styled

--- src/Components/Taskbar/Taskbar.jsx
+++ src/Components/Taskbar/Taskbar.jsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { Discord, Github, LinkedIn } from "./Svg/Svg";
+import { Theme } from "../../Themes/Webpage/Theme";
+import { Discord, Github, LinkedIn, ChangeThemeBtn } from "./Svg/Svg";
 
 function Taskbar() {
+  const styles = {
+    footer: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr 1fr',
+      placeItems: 'center',
+      margin: '0 40px'
+    },
+    svgBtn: {
+      backgroundColor: Theme.primaryColor
+    },
+    socialLinks: {
+      width: '200px',
+      display: 'flex',
+      justifyContent: 'space-evenly'
+    },
+    changeTheme: {
+      justifySelf: 'flex-end'
+    }
+  }
   return (
-    <>
-      <button className="linked-in"><LinkedIn/></button>
-      <button className="github"><Github/></button>
-      <button className="discord"><Discord/></button>
-    </>
+    <footer style={styles.footer}>
+      <div></div>
+      <div className="social-links" style={styles.socialLinks}>
+        <button className="linked-in" style={styles.svgBtn}><LinkedIn/></button>
+        <button className="github" style={styles.svgBtn}><Github/></button>
+        <button className="discord" style={styles.svgBtn}><Discord/></button>
+      </div>
+      <button style={{...styles.svgBtn, ...styles.changeTheme}}><ChangeThemeBtn/></button>
+    </footer>
   )
 }
 
