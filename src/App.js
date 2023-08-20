352c9214405da94865d8e737a3fce81620182d2b
Valerian Coelho <valerian27012004@gmail.com>
2023-08-20 16:32:47 +0530

    Styles Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { Theme } from "./Themes/Webpage/Theme";
 import Navbar from "./Components/Navbar/Navbar";
 import Center from "./Components/Center/Center";
 import Taskbar from "./Components/Taskbar/Taskbar";
 
 function App() {
+  const styles = {
+    body: {
+      backgroundColor: Theme.primaryColor,
+      height: '100vh'
+    }
+  }
+
   return (
-    <>
+    <div style={styles.body}>
       <Navbar/>
       <Center/>
       <Taskbar/>
-    </>
+    </div>
   );
 }
 
