773b25197b993f05183b33c8c08ad3009ccf36bd
Valerian Coelho <valerian27012004@gmail.com>
2023-08-20 16:32:26 +0530

    Styles Added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,46 @@
 import React from "react";
+import { Theme } from "../../Themes/Webpage/Theme";
 import { Logo, About } from "./Svg/Svg";
  
 function Navbar() {
+  const styles = {
+    header: {
+      display: 'flex',
+      justifyContent: 'center'
+    },
+    titleSection: {
+      display: 'flex',
+      alignItems: 'center'
+    },
+    navigationSection: {
+      display: 'flex',
+      alignItems: 'center'
+    },
+    shareBtn: {
+      backgroundColor: Theme.shareBtnBgColor,
+      color: Theme.shareBtnColor
+    },
+    donateBtn: {
+      backgroundColor: Theme.donateBtnBgColor,
+      color: Theme.donateBtnColor
+    },
+    btn: {
+      width: '90px',
+      height: '40px',
+      borderRadius: '10px'
+    }
+  }
+
   return (
-    <header>
-      <div className="title-section">
+    <header style={styles.header}>
+      <div className="title-section" style={styles.titleSection}>
           <Logo/>
         <div className="title">Linux Theme Formatter</div>
       </div>
-      <nav className="navigation-section">
-        <div className="share-btn">Share</div>
-        <div className="donate-btn">Donate</div>
-        <About/>
+      <nav style={styles.navigationSection}>
+        <button style={{...styles.shareBtn, ...styles.btn}}>Share</button>
+        <button style={{...styles.donateBtn, ...styles.btn}}>Donate</button>
+        <button className="about"><About/></button>
       </nav>
     </header>
   )
