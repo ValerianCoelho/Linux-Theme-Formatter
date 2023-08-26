111c13594858b618d5d7c9e71c8dc926bdf00c3e
Valerian Coelho <valerian27012004@gmail.com>
2023-08-26 21:31:41 +0530

    Styling Method Changed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,51 +1,65 @@
 import React from "react";
-import { Theme } from "../../Themes/Theme";
+import { connect } from "react-redux";
 import { Logo, About } from "./Svg/Svg";
  
-function Navbar() {
-  const styles = {
-    header: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      margin: '0 40px 20px 40px'
-    },
-    titleSection: {
-      display: 'flex',
-      alignItems: 'center'
-    },
-    navigationSection: {
-      display: 'flex',
-      alignItems: 'center',
-      gap: '20px'
-    },
-    shareBtn: {
-      backgroundColor: Theme.shareBtnBgColor,
-      color: Theme.shareBtnColor
-    },
-    donateBtn: {
-      backgroundColor: Theme.donateBtnBgColor,
-      color: Theme.donateBtnColor
-    },
-    btn: {
-      width: '90px',
-      height: '40px',
-      borderRadius: '10px'
+function Navbar(props) {
+  const styles = `
+    .header {
+      display: flex;
+      justify-content: space-between;
+      margin: 0 40px 20px 40px;
     }
-  }
-
+    .title-section {
+      display: flex;
+      align-items: center;
+      color: ${props.Theme.pageFgColor};
+    }
+    .navigation-section {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+    }
+    .share-btn {
+      color: ${props.Theme.shareBtnFgColor};
+      background-color: ${props.Theme.shareBtnBgColor};
+    }
+    .donate-btn {
+      color: ${props.Theme.donateBtnFgColor};
+      background-color: ${props.Theme.donateBtnBgColor};
+    }
+    .btn {
+      width: 90px;
+      height: 40px;
+      border-radius: 10px;
+    }
+    .about {
+      background-color: ${props.Theme.pageBgColor}
+    }
+  `
   return (
-    <header style={styles.header}>
-      <div className="title-section" style={styles.titleSection}>
-          <Logo/>
-        <pre className="title">  Linux Theme Formatter</pre>
-      </div>
-      <nav style={styles.navigationSection}>
-        <button style={{...styles.shareBtn, ...styles.btn}}>Share</button>
-        <button style={{...styles.donateBtn, ...styles.btn}}>Donate</button>
-        <button className="about"><About/></button>
-      </nav>
-    </header>
+    <>
+      <style> {styles} </style>
+      <header className="header">
+        <div className="title-section">
+          <Logo color={props.Theme.pageFgColor}/>
+          <pre className="title">  Linux Theme Formatter</pre>
+        </div>
+        <nav className="navigation-section">
+          <button className="btn share-btn">Share</button>
+          <button className="btn donate-btn">Donate</button>
+          <button className="about"><About color={props.Theme.pageFgColor}/></button>
+        </nav>
+      </header>
+    </>
   )
 }
 
-export default Navbar;
+const mapStateToProps = state => {
+  return {
+    Theme: state.Theme
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(Navbar);
