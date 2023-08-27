93db3a4555abcbaf8bcf793eff5a8f62a0fed18c
Valerian Coelho <valerian27012004@gmail.com>
2023-08-27 14:00:48 +0530

    About Button Working

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Logo, About } from "./Svg/Svg";
+import { Logo, About, Close } from "./Svg/Svg";
  
 function Navbar(props) {
+  const [isAboutActive, setIsAboutActive] = useState(false)
   const styles = `
     .header {
       display: flex;
@@ -36,8 +37,43 @@ function Navbar(props) {
       align-items: center;
       text-decoration: none;
     }
-    .about {
+    .about-btn {
       background-color: ${props.Theme.pageBgColor}
+    }
+    .overlay {
+      display: none;
+    }
+    .overlay-active {
+      background-color: ${props.Theme.pageBgColor};
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background-color: rgba(0 ,0 ,0 , 0.5);
+      backdrop-filter: blur(10px);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .about-window {
+      width: 60%;
+      color: ${props.Theme.pageFgColor};
+      background-color: ${props.Theme.pageBgColor};
+      padding: 3rem;
+      border-radius: 30px 20px;
+      font-size: 18px;
+    }
+    .heading {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 10pxl
+    }
+    button {
+      background-color: ${props.Theme.pageBgColor};
+    }
+    h2, h3, p {
+      font-family: 'Ubuntu Mono', monospace;
     }
   `
   return (
@@ -51,8 +87,32 @@ function Navbar(props) {
         <nav className="navigation-section">
           <a href={`https://api.whatsapp.com/send?text=' + ${encodeURIComponent(window.document.title + ' - ' + window.location.href)}`} target="_blank" className="btn share-btn">Share</a>
           <a href="https://www.buymeacoffee.com/vc27" target="_blank" className="btn donate-btn">Donate</a>
-          <button  className="about"><About color={props.Theme.pageFgColor}/></button>
+          <button className="about-btn" onClick={()=>{
+            setIsAboutActive(true)
+          }}><About color={props.Theme.pageFgColor}/></button>
         </nav>
+        <div className={isAboutActive ? 'overlay-active' : 'overlay'}>
+          <div className="about-window">
+            <div className="heading">
+              <h2>Linux Theme Formatter</h2>
+              <button onClick={()=>{
+                setIsAboutActive(false)
+              }}><Close color={props.Theme.pageFgColor}/></button>
+            </div>
+            <p>
+              Greetings, fellow code wranglers and Linux enthusiasts! 🐧🎩<br/><br/>
+              We know the drill: practical lectures, Ubuntu screens, and the elusive clock hands. Fear not, for we're here to transform your code outputs into visual masterpieces. <br/><br/>
+              Enter Linux Theme Formatter – the genie that turns text into terminal-style artistry.<br/><br/>
+              Not everyone has a Linux wonderland at home, and running those terminal commands might be a distant dream. But why just dream when you can "theme"?<br/><br/>
+              With Linux Theme Formatter, all you need is a dash of text magic, and voilà! Enter your output text, sprinkle it with the enchanting colours of the Linux terminal, and watch the magic unfold. 🎨🚀<br/><br/>
+            </p>
+            <h3 style={{ marginBottom: '10px' }}>Note</h3>
+            <p>
+              Some features might be taking a coffee break at the moment, but stay tuned for the full experience soon. Your text-to-terminal transformation is on the horizon!<br/><br/>
+              Thank-you for your patience
+            </p>
+          </div>
+        </div>
       </header>
     </>
   )
